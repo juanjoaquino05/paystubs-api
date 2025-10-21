52ca363aca41eabfcb1f9d883880c762f5194393
Juan Henriquez <juanjoaquino05@gmail.com>
2025-10-21 19:04:06 -0400

    Creating initial Springboot project

---
+++ src/test/java/com/atdev/paystubs_api/PaystubsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atdev.paystubs_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystubsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
